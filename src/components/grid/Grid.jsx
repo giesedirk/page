f65d20521ac979e5c0c48bb665321ca6a4b8f0d5
dirkgiese <dirk.giese70@gmail.com>
2025-12-01 06:11:41 +0100

    add display grid

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,22 +1,27 @@
+import {gridCells} from '../../constants/index.js'
 export const Grid = () => {
-	
-	const gridItems = []
-	
 	const Image = ({...props}) => {
-		console.log(props)
-		//width: The desired width of the image
-		//height: The desired height of the image
-		//image:Get a specific image
-		//id:Set a specific id
-		//classes:Add custom classes to the image
-		//const Endpoint = `https://picsum.photos/id/${image}/${width}/${height}`
-		//return <img src={Endpoint} alt="bild" className={classes}/>
+		const {image, width, height, classes = 'grid__img'} = props
+		const Endpoint = `https://picsum.photos/id/${image}/${width}/${height}`
+		return <img src={Endpoint} alt="bild" className={classes}/>
 	}
-	
+
 	return (
 		<>
-			<div className="grid">
-				<Image width={320} height={280} image={1}/>
+			<div className="grid">{
+				gridCells.map((cell, idx) => {
+					const Klasse = `grid__cell grid__cell--${idx + 1}`
+					return (
+						<div className={Klasse} key={idx}>
+							<Image
+								width={cell.width}
+								height={cell.height}
+								image={cell.image}
+							/>
+						</div>
+					)
+				})
+			}
 			</div>
 		</>
 	)
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+//define Grid Images as ...
+export const gridCells = [{
+	width: 680,
+	height: 620,
+	image: 10,
+}, {
+	width: 320,
+	height: 280,
+	image: 11,
+}, {
+	width: 320,
+	height: 340,
+	image: 12,
+}, {
+	width: 320,
+	height: 440,
+	image: 13,
+}, {
+	width: 320,
+	height: 280,
+	image: 14,
+}]
